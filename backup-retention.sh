f98247efe05020417d593a0df54c1982ea73f6a1
devtedlee <persisne@gmail.com>
2023-03-03 09:44:35 +0900

    Update backup-retention.sh
    fix logic

--- backup-retention.sh
+++ backup-retention.sh
@@ -1,68 +1,118 @@
 #!/bin/bash
 
-# Command execute example
-# ./backup-retention.sh --git-repo-urls "https://{username}:{passowrd}@github.com/myusername/myrepo.git" "https://{personal_access_token}@github.com/myusername/myotherrepo.git" --retention-days 30 --backup-expired-years 1
+# execute example:
+# chmod +x backup-retention.sh
+# ./backup-retention.sh --retentionDays 7 --backupExpiredYears 2 --backupRootDir /backups --gitRepoUrls https://github.com/username1/repo1 https://github.com/username2/repo2
 
-# Parse command line arguments
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --git-repo-urls) git_repo_urls=("$2"); shift ;;
-        --retention-days) retention_days="$2"; shift ;;
-        --backup-expired-years) backup_expired_years="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+# Define default values
+retentionDays=30
+backupExpiredYears=1
+backupRootDir=/backups
+
+# Parse named command line arguments
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -r|--retentionDays)
+        retentionDays="$2"
+        shift
+        shift
+        ;;
+        -b|--backupExpiredYears)
+        backupExpiredYears="$2"
+        shift
+        shift
+        ;;
+        -d|--backupRootDir)
+        backupRootDir="$2"
+        shift
+        shift
+        ;;
+        -g|--gitRepoUrls)
+        shift
+        gitRepoUrls=("$@")
+        break
+        ;;
+        *)
+        echo "Unknown argument: $1"
+        exit 1
+        ;;
     esac
-    shift
 done
 
 # Validate required command line arguments
-if [[ -z "$git_repo_urls" ]]; then
-    echo "Please provide at least one Git repository URL with --git-repo-urls."
+if [ -z "${gitRepoUrls}" ]; then
+    echo "Please provide at least one Git repository URL with -g or --gitRepoUrls."
     exit 1
 fi
 
-if [[ -z "$retention_days" ]]; then
-    echo "Please provide a retention period in days with --retention-days."
+if [ -z "${retentionDays}" ]; then
+    echo "Please provide a retention period in days with -r or --retentionDays."
     exit 1
 fi
 
-if [[ -z "$backup_expired_years" ]]; then
-    echo "Please provide a backup expired period in years with --backup-expired-years."
+if [ -z "${backupExpiredYears}" ]; then
+    echo "Please provide a backup expired period in years with -b or --backupExpiredYears."
     exit 1
 fi
+
+function create_directory_if_not_exists {
+    directoryName=$1
+
+    if [ ! -d "${directoryName}" ]; then
+        echo "Creating directory: ${directoryName}"
+        mkdir -p "${directoryName}"
+    fi
+}
+
+# Create backups root directory if it doesn't exist
+create_directory_if_not_exists "${backupRootDir}"
+
+# Move to backup directory
+cd "${backupRootDir}" || exit
 
 # Function to clean up old backup files
 function clean_up_files {
+
+    # Move to backup directory
+    cd "${backupRootDir}" || exit
+
     # Create a new directory with today's date
-    backup_dir=$(date '+%Y-%m-%d')
-    mkdir "$backup_dir"
+    backupDir=$(date +%Y-%m-%d)
+    mkdir "${backupDir}"
 
     # Move all tar.gz files to the new directory
-    mv *.tar.gz "$backup_dir"
+    mv *.tar.gz "${backupDir}"
 
-    # Remove directories older than backup_expired_years
-    find . -maxdepth 1 -type d -name "20*" -mtime +"$((backup_expired_years*365))" -exec rm -rf {} \;
+    # Remove directories older than backupExpiredYears
+    find . -type d -name "20*" -mtime +$((365*backupExpiredYears)) -exec rm -rf {} \;
 }
 
 # Clone each Git repository URL and create a backup archive
-for git_repo_url in "${git_repo_urls[@]}"; do
+for gitRepoUrl in "${gitRepoUrls[@]}"
+do
     # Clone the repository
-    git clone --depth 1 "$git_repo_url" || exit 1
+    git clone --depth 1 "${gitRepoUrl}"
 
     # Get the name of the repository
-    repo_name=$(echo "$git_repo_url" | sed 's/.*\///' | sed 's/\.git//')
+    repoName=$(echo "${gitRepoUrl}" | awk -F'/' '{print $NF}' | sed 's/\.git$//')
 
-    # Zip the repository folder
-    tar -czvf "${repo_name}.tar.gz" "$repo_name" || exit 1
+    create_directory_if_not_exists "./${repoName}"
 
-    # Remove the unzipped repository folder
-    rm -rf "$repo_name" || exit 1
+    # Tar and gzip the repository folder
+    tar -zcvf "${repoName}.tar.gz" "${repoName}"
+
+    # Remove the cloned repository folder
+    rm -rf "${repoName}"
 done
 
 # Clean up old backup files
 clean_up_files
 
-# Define and start Ubuntu service
-systemctl start backup-retention.service
-
-# Set retention date
-systemctl --now set-property backup-retention.service 'TimeoutStopSec' "${retention_days}s"
+# Create cron job for backup-retention.sh
+if ! crontab -l | grep -q "${backupRootDir}/backup-retention.sh"; then
+  (crontab -l 2>/dev/null; echo "0 0 */${retentionDays} * * $(which bash) $(pwd)/backup-retention.sh -g ${gitRepoUrls[*]} -r '${retentionDays}' -b '${backupExpiredYears}' -d '${backupRootDir}'") | sort -u | crontab -
+  echo "Cron job created or updated successfully."
+fi
